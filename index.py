494f915337d538ffca8e62bb7a6328c9c4e5122b
XinYuJune <74338675+XinYuJune@users.noreply.github.com>
2021-10-28 17:39:02 +0800

    Update index.py

--- index.py
+++ index.py
@@ -93,7 +93,7 @@ MAX_Captcha_Times = 20
 
 
 class Util:  # 统一的类
-    logs = 'V2021.6.17'
+    logs = 'V2021.6.17'+'\n'+USERNAME
     OCRclient = None
 
     @staticmethod
@@ -774,7 +774,7 @@ def main():
     }
     Do(School_Server_API, user)
     if (PUSH_LEVEL > 1):
-        Util.SendMessage(USERNAME, Util.logs)
+        Util.SendMessage('签到日志', Util.logs)
 # 提供给腾讯云函数调用的启动函数
 
 
